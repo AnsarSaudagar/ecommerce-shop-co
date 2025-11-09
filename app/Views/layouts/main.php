a18a4e4686c25faf9af941ee68627cf1a6258d32
ansar <ansarsaudagar40@gmail.com>
2025-11-09 18:24:41 +0530

    added partial view for navbar

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -19,23 +19,8 @@
 </head>
 
 <body>
-    <nav id="navbar">
-        <p id="main-logo">SHOP.CO</p>
-        <ul>
-            <li>Shop<i style="margin-top: 4px; margin-left: 5px;" class="ph ph-caret-down"></i></li>
-            <li>On Sale</li>
-            <li>New Arrivals</li>
-            <li>Brand</li>
-        </ul>
-        <div id="search-bar">
-            <i class="ph ph-magnifying-glass"></i>
-            <p>Search For Products...</p>
-        </div>
-        <ul id="actions">
-            <li><i class="ph ph-shopping-cart-simple"></i></li>
-            <li><i class="ph ph-user-circle"></i></li>
-        </ul>
-    </nav>
+    
+    <?= $this->include('partials/navbar') ?>
     <main>
         <?= $content ?? '' ?>
     </main>
