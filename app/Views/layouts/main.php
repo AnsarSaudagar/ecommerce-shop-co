e6a446b28c9990b9021207add1535ecb26c0e254
ansar <ansarsaudagar40@gmail.com>
2025-11-09 17:31:53 +0530

    created css for home and change structure

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/satoshi-fonts.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/satoshi-fonts.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/home.css'); ?>">
     <title>Shop.co</title>
 </head>
 <body>
-    <h1>Hello world</h1>
+    <nav>
+        Navbar
+    </nav>
     <main>
         <?= $content ?? '' ?>
     </main>
 </body>
 </html>
+
