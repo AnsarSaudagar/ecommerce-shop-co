e4d8a0067bf79dfd4fd7d9f758f8c7eb9eaab129
ansar <ansarsaudagar40@gmail.com>
2025-11-10 23:47:25 +0530

    header - (mobile design) ready

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url('css/satoshi-fonts.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/common.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/home.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/navbar.css'); ?>">
     <link
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,1 +1,26 @@
-<!-- ansar -->
+<header>
+    <div>
+        <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+        <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+        <button>Shop Now</button>
+        <div class="data-display first-row">
+            <div class="data item1">
+                <h2>200+</h2>
+                <p>International Brands</p>
+            </div>
+            <div class="data item2">
+                <h2>2000+</h2>
+                <p>High-Quality Products</p>
+            </div>
+        </div>
+        <div class="data-display">
+            <div class="data item3">
+                <h2>30000+</h2>
+                <p>Happy Customers</p>
+            </div>
+        </div>
+    </div>
+    <div class="display-banner">
+        <img src="<?= base_url() . 'images/home/main-banner.png'?>" alt="">
+    </div>
+</header>
