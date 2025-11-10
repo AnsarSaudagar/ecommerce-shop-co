57b102eb7d45aceb897b9b113dab7c6e75c6c402
ansar <ansarsaudagar40@gmail.com>
2025-11-11 00:16:48 +0530

    basic structure ready

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,22 +1,26 @@
 <header>
-    <div>
-        <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
-        <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-        <button>Shop Now</button>
-        <div class="data-display first-row">
-            <div class="data item1">
-                <h2>200+</h2>
-                <p>International Brands</p>
-            </div>
-            <div class="data item2">
-                <h2>2000+</h2>
-                <p>High-Quality Products</p>
-            </div>
+    <div class="display-content">
+        <div class="display-content__data">
+            <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+            <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+            <button>Shop Now</button>
         </div>
-        <div class="data-display">
-            <div class="data item3">
-                <h2>30000+</h2>
-                <p>Happy Customers</p>
+        <div class="display-stats">
+            <div class="data-display first-row">
+                <div class="data item1">
+                    <h2>200+</h2>
+                    <p>International Brands</p>
+                </div>
+                <div class="data item2">
+                    <h2>2000+</h2>
+                    <p>High-Quality Products</p>
+                </div>
+            </div>
+            <div class="data-display">
+                <div class="data item3">
+                    <h2>30000+</h2>
+                    <p>Happy Customers</p>
+                </div>
             </div>
         </div>
     </div>
